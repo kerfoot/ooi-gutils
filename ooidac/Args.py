e27fe4067913f9b58511c323d85cf6ac8c2b4d63
John Kerfoot <johnkerfoot@gmail.com>
2017-03-26 13:40:06 -0400

    added additional props

--- ooidac/Args.py
+++ ooidac/Args.py
@@ -14,11 +14,49 @@ class Args(object):
         self._instruments = instruments
         self._response_file = None
         self._glider_config_path = None
+        self._user = 'rutgers'
+        self._password = '1ioo$man9'
+        self._host = 'data.ioos.us'
+        self._deployment_nc_path = None
+       
+    @property
+    def deployment_nc_path(self):
+        return self._deployment_nc_path
+    @deployment_nc_path.setter
+    def deployment_nc_path(self, path):
+        self._deployment_nc_path = path
+        
+    @property
+    def glider_deployment_path(self):
+        return self._glider_config_path
+    @glider_deployment_path.setter
+    def glider_deployment_path(self, path):
+        self._glider_config_path = path
+         
+    @property
+    def user(self):
+        return self._user
+    @user.setter
+    def user(self, user):
+        self._user = user
+        
+    @property
+    def password(self):
+        return self._password
+    @password.setter
+    def password(self, pw):
+        self._password = pw
+        
+    @property
+    def host(self):
+        return self._host
+    @host.setter
+    def host(self, host):
+        self._host = host
         
     @property
     def glider_config_path(self):
         return self._glider_config_path
-        
     @glider_config_path.setter
     def glider_config_path(self, glider_config_path):
         self._glider_config_path = glider_config_path
@@ -34,7 +72,6 @@ class Args(object):
     @property
     def instruments(self):
         return self._instruments
-
     @instruments.setter
     def instruments(self, instruments):
         if type(instruments) == str:
@@ -45,7 +82,6 @@ class Args(object):
     @property
     def root(self):
         return self._root
-
     @root.setter
     def root(self, directory):
         self._root = directory
@@ -53,7 +89,6 @@ class Args(object):
     @property
     def base_url(self):
         return self._base_url
-        
     @base_url.setter
     def base_url(self, base_url):
         self._base_url = base_url
@@ -61,7 +96,6 @@ class Args(object):
     @property
     def loglevel(self):
         return self._loglevel
-        
     @loglevel.setter
     def loglevel(self, loglevel):
         self._loglevel = loglevel
@@ -69,7 +103,6 @@ class Args(object):
     @property
     def status(self):
         return self._status
-        
     @status.setter
     def status(self, status):
         self._status = status
@@ -77,7 +110,6 @@ class Args(object):
     @property
     def debug(self):
         return self._debug
-        
     @debug.setter
     def debug(self, debug):
         self._debug = debug
@@ -85,7 +117,6 @@ class Args(object):
     @property
     def timeout(self):
         return self._timeout
-        
     @timeout.setter
     def timeout(self, timeout):
         self._timeout = timeout
