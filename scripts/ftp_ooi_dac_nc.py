3b416912c892fd9c3d294323aa9dd3e223a2e53c
John Kerfoot <johnkerfoot@gmail.com>
2017-04-06 22:46:52 -0400

    call ooidac.ftp.ftp_trajectory_nc_to_dac to ftp files

--- scripts/ftp_ooi_dac_nc.py
+++ scripts/ftp_ooi_dac_nc.py
@@ -3,9 +3,13 @@
 import os
 import logging
 import argparse
-import ftputil
 import sys
 import glob
+import json
+import pytz
+import shutil
+from ooidac.ftp import ftp_trajectory_nc_to_dac
+from dateutil import parser
 
 def main(args):
     """FTP NetCDF files contained in the specified directory to the U.S IOOS
@@ -18,19 +22,7 @@ def main(args):
         log_format = '%(asctime)s:%(funcName)s:%(module)s:[line %(lineno)d]:%(levelname)s:%(message)s'
     logging.basicConfig(format=log_format, level=log_level)
     
-    if not args.deployment_nc_path:
-        logging.error('No glider deployment path specified')
-        return 1
-    if not os.path.isdir(args.deployment_nc_path):
-        logging.error('Invalid glider deployment path specified {:s}'.format(args.deployment_nc_path))
-        return 1
-        
-    nc_files = glob.glob(os.path.join(args.deployment_nc_path, '*.nc'))
-    if not nc_files:
-        logging.info('No local NetCDF files found {:s}'.format(args.deployment_nc_path))
-        return 0
-        
-    # Set up the FTP host, user and password arguments
+    # Set up and validate the FTP host, user and password arguments
     args.host = args.host or os.getenv('OOI_GLIDER_DAC_FTP_HOST')
     args.user = args.user or os.getenv('OOI_GLIDER_DAC_USER')
     args.password = args.password or os.getenv('OOI_GLIDER_DAC_PASSWORD')
@@ -47,55 +39,93 @@ def main(args):
         logging.error('No FTP password specified')
         return 1
         
-    # Connect to the FTP server
-    try:
-        ftp_host = ftputil.FTPHost(args.host, args.user, args.password)
-        # Synchronize times between host and local machines
-        ftp_host.synchronize_times()
-    except ftputil.FTPError as e:
-        logging.error('ftputil {:s}'.format(e))
-        
-    trajectory = os.path.basename(args.deployment_nc_path)
-    
-    # Get the list of trajectory directories on the server
-    ftp_trajectories = ftp_host.listdir('.')
-    if trajectory not in ftp_trajectories:
-        logging.warning('Deployment has not been registered {:s}'.format(trajectory))
-        ftp_host.close()
+    if not args.glider_deployment_path:
+        logging.error('No glider deployment path specified')
+        return 1
+    if not os.path.isdir(args.glider_deployment_path):
+        logging.error('Invalid glider deployment path specified {:s}'.format(args.glider_deployment_path))
         return 1
         
-    # Change to the remote trajectory directory
-    ftp_host.chdir(trajectory)
-    
-    if not args.all:
-        remote_files = ftp_host.listdir('.')
-        
-    exit_status = 0
-    for nc_file in nc_files:
-        try:
-            if args.all:
-                ftp_host.upload(nc_file, '.')
-            elif args.update:
-                ftp_host.upload_if_newer(nc_file, '.')
-            else:
-                if os.path.basename(nc_file) in remote_files:
-                    continue
-                ftp_host.upload(nc_file, '.')
-                
-            logging.debug('File uploaded {:s}'.format(nc_file))
-        except ftputil.FTPError as e:
-            logging.error('Error uploading {:s} ({:s})'.format(nc_file, e))
-            exit_status = 1
-            continue
+    # Create and validate the deployment config path
+    deployment_cfg_path = os.path.join(args.glider_deployment_path, 'cfg')
+    if not os.path.isdir(deployment_cfg_path):
+        logging.error('Deployment configuration directory does not exist {:s}'.format(deployment_cfg_path))
+        return 1
+    # Create, validate and load the deployment.json file
+    deployment_cfg_file = os.path.join(deployment_cfg_path, 'deployment.json')
+    if not os.path.isfile(deployment_cfg_file):
+        logging.warning('Invalid deployment configuration file {:s}'.format(deployment_cfg_file))
+        return 1
+    try:
+        with open(deployment_cfg_file, 'r') as fid:
+            deployment = json.load(fid)
+    except OSError as e:
+        logging.error('Error loading deployment configuration file {:s}'.format(deployment_cfg_file))
+        return 1
             
-    return exit_status
+    if 'trajectory_date' not in deployment:
+        logging.warning('Deployment is missing trajectory_date {:s}'.format(deployment_cfg_file))
+        return 1
+    if 'glider' not in deployment:
+        logging.warning('Deployment is missing glider name {:s}'.format(deployment_cfg_file))
+        return
+        
+    # Create the DAC NetCDF trajectory name
+    try:
+        traj_dt = parser.parse(deployment['trajectory_date']).replace(tzinfo=pytz.UTC)
+    except ValueError as e:
+        logging.error('Error parsing deployment trajectory_date: {:s}'.format(deployment['trajectory_date']))
+        return 1
+    trajectory = '{:s}-{:s}'.format(deployment['glider'], traj_dt.strftime('%Y%m%dT%H%M'))
+    # Location of local NetCDF files to be ftp'd
+    nc_source_dir = os.path.join(args.glider_deployment_path, trajectory)
+    logging.info('Local NetCDF source directory: {:s}'.format(nc_source_dir))
+    if not os.path.isdir(nc_source_dir):
+        logging.warning('Local NetCDF directory does not exist: {:s}'.format(nc_source_dir))
+        return 1
+    nc_archive_dir = os.path.join(args.glider_deployment_path, 'nc-archive')
+    logging.info('Local NetCDF archive directory: {:s}'.format(nc_archive_dir))
+    if not os.path.isdir(nc_archive_dir):
+        logging.info('Creating archive {:s}'.format(nc_archive_dir))
+        try:
+            os.mkdir(nc_archive_dir)
+        except OSError as e:
+            logging.error('Error creating archive: {:s} ({:s})'.format(nc_archive_dir, 3))
+            continue
+        
+    nc_files = glob.glob(os.path.join(nc_source_dir, '*.nc'))
+    if not nc_files:
+        logging.info('No local NetCDF files found {:s}'.format(args.deployment_nc_path))
+        return 0
+        
+    # FTP the files
+    ftp_files = ftp_trajectory_nc_to_dac(args.host,
+        args.user,
+        args.password,
+        trajectory,
+        nc_files,
+        update=args.update)
+    for nc_file in ftp_files:
+        logging.info('Archiving file to {:s}'.format(nc_archive_dir))
+        try:
+            shutil.move(nc_file, nc_archive_dir)
+            if args.verbose:
+                sys.stdout.write('{:s}\n'.format(nc_file))
+        except OSError as e:
+            logging.warning('Error archiving file {:s} ({:s})'.format(nc_file, e))
+            
+    if len(ftp_files) == len(nc_files):
+        return 0
+    else:
+        return 1
+
     
 if __name__ == '__main__':
     
     arg_parser = argparse.create_arg_parser(description=main.__doc__)
     
-    arg_parser.add_argument('deployment_nc_path',
-        help='Location of NetCDF files to be uploaded')
+    arg_parser.add_argument('glider_deployment_path',
+        help='Path to glider deployment configuration information')
     
     arg_parser.add_argument('-h', '--host',
         help='FTP host url')
@@ -106,12 +136,16 @@ if __name__ == '__main__':
     arg_parser.add_argument('-p', '--password',
         help='Password')
         
-    arg_parser.add_argument('--update',
+    arg_parser.add_argument('-u', '--update',
         help='Re-transfer file if a newer version exists locally',
         action='store_true')
         
     arg_parser.add_argument('-a', '--all',
         help='Transfer all files regardless of whether they exist on the remote',
+        action='store_true')
+        
+    arg_parser.add_argument('-v', '--verbose',
+        help='Print the list of successfully transferred files to STDOUT',
         action='store_true')
         
     arg_parser.add_argument('-l', '--loglevel',
